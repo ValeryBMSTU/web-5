3a882bc40356aae55faa8f4d8575b92dfbaa2683
ZeusKronosovich <Korotaev6776@mail.com>
2024-09-20 18:25:33 +0300

    51

--- projects/calculator/main.go
+++ projects/calculator/main.go
@@ -1,7 +1,31 @@
 package main
 
-// реализовать calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+import (
+	"fmt"
+)
 
+func calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int {
+	output := make(chan int)
+	go func(ch chan int) {
+		defer close(ch)
+		select {
+		case x := <-firstChan:
+			ch <- x * x
+			fmt.Println("firchan worked")
+		case x := <-secondChan:
+			ch <- x * 3
+			fmt.Println("secchan worked")
+		case <-stopChan:
+			fmt.Println("stopchan worked")
+		}
+	}(output)
+	return output
+}
 func main() {
-	// здесь должен быть код для проверки правильности работы функции calculator(firstChan <-chan int, secondChan <-chan int, stopChan <-chan struct{}) <-chan int
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	stop := make(chan struct{})
+	result := calculator(ch1, ch2, stop)
+	ch2 <- 2
+	fmt.Println(<-result)
 }
